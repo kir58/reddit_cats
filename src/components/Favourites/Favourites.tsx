a352a136bd4277d6b60c2ce42cc19c94907db147
Kir Kup <k.bushmin@andersenlab.com>
2020-11-07 16:12:25 +0300

    rewrite FavouritesComponet by hooks

--- src/components/Favourites/Favourites.tsx
+++ src/components/Favourites/Favourites.tsx
@@ -1,6 +1,6 @@
-import React from 'react';
+import React, { useState, useEffect, ReactElement } from 'react';
 import axios from 'axios';
-import { connect, ConnectedProps } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
 import styles from './Favourites.css';
 import * as actions from '../../actions';
@@ -11,81 +11,59 @@ import { CatDetail } from '../../types';
 
 
 
-const mapStateToProps = (state: RootState) => ({
-  favouritesCatsIds: state.favouritesCatsIds,
-});
+const Favourites = () : ReactElement =>  {
+  const [cats, setCats] = useState<CatDetail[]>([]);
+  const [fetchingState, setFetchingState] = useState<string>('none');
 
-const actionsCreators = {
-  toggleFavouritePosts: actions.toggleFavouritePosts,
-};
+  const dispatch = useDispatch();
+  const { favouritesCatsIds } = useSelector((state: RootState) => state);
 
-const connector = connect(
-  mapStateToProps,
-  actionsCreators,
-)
+  useEffect(() => {
+    const getFavouritesCats = async () => {
+      setFetchingState('requested');
+      try {
+        const response = await axios.get(`https://www.reddit.com/api/info.json?id=${favouritesCatsIds.join(',')}`);
+        setFetchingState('finished');
+        setCats(response.data.data.children);
+      } catch (e) {
+        setFetchingState('failed');
+        setCats([]);
+      }
+    };
 
-type PropsFromRedux = ConnectedProps<typeof connector>
-
-
-type StateTypes = {
-  cats: CatDetail[],
-  fetchingState: string,
-}
-
-class Favourites extends React.Component<PropsFromRedux> {
-  state : StateTypes = { cats: [], fetchingState: 'none' }
-
-  componentDidMount() {
-    this.getFavouritesCats();
-  }
-
-  getFavouritesCats = async () => {
-    const { favouritesCatsIds } = this.props;
-    this.setState({ fetchingState: 'requested' });
-    try {
-      const response = await axios.get(`https://www.reddit.com/api/info.json?id=${favouritesCatsIds.join(',')}`);
-      this.setState({ cats: response.data.data.children, fetchingState: 'finished' });
-    } catch (e) {
-      this.setState({ fetchingState: 'failed' });
-    }
-  }
-
-  handleToggle = (name: string) => () => {
-    const { cats } = this.state;
-    const { toggleFavouritePosts } = this.props;
-    this.setState({ cats: cats.filter((el) => el.data.name !== name) });
-    toggleFavouritePosts({ name });
-  }
-
-  render() {
-    const { favouritesCatsIds } = this.props;
-    const { cats, fetchingState } = this.state;
-
-    if (fetchingState === 'requested') {
-      return <Loader byCenter />;
-    }
-
-    if (fetchingState === 'failed') {
-      return 'reload the page please';
-    }
+    getFavouritesCats();
+  }, []);
   
-    if (cats.length === 0) {
-      return (
-        <div>
-          Your favourites is empty. You can add cute cats
-          <Link to="/" className={styles.link}> here</Link>
-        </div>
-      );
-    }
+  const handleToggle = (name: string) => () => {
+    const updCats = cats.filter((el) => el.data.name !== name)
+    setCats(updCats);
+    dispatch(actions.toggleFavouritePosts({ name }));
+  }
 
+  if (fetchingState === 'requested') {
+    return <Loader byCenter />;
+  }
+
+  if (fetchingState === 'failed') {
+    return <div>reload the page please</div>;
+  }
+
+  if (cats.length === 0) {
     return (
-      <Posts
-        cats={cats}
-        favouritesCatsIds={favouritesCatsIds}
-        onToggle={this.handleToggle}
-      />
+      <div>
+        Your favourites is empty. You can add cute cats
+        <Link to="/" className={styles.link}> here</Link>
+      </div>
     );
   }
+
+  return (
+    <Posts
+      cats={cats}
+      favouritesCatsIds={favouritesCatsIds}
+      onToggle={handleToggle}
+    />
+  );
 }
 
-export default connector(Favourites);
+export default Favourites;
